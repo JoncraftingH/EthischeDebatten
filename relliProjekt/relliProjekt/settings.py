b2779f49a0e3495ea06ef2304ad1fc8866b01079
Joncrafting <jonas3.hck@gmail.com>
2025-06-11 16:58:55 +0200

    changed settings

--- relliProjekt/relliProjekt/settings.py
+++ relliProjekt/relliProjekt/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://ethischedebatten.onrender.com', "*"]
+CSRF_TRUSTED_ORIGINS = ['https://ethischedebatten.onrender.com']
 
 
 # Application definition
